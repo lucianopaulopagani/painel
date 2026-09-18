1bbf2e81191f7fdafca01d648f49c75f2a411b19
enter-pro <github@enter.pro>
2026-09-18 18:11:52 +0000

    project_d3a8c731c16c40afb612732f74c10df3_turn_227_1789755112380 commit message

--- src/components/admin/CompanyBulkEditDialog.tsx
+++ src/components/admin/CompanyBulkEditDialog.tsx
@@ -134,7 +134,7 @@ export default function CompanyBulkEditDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-h-[90vh] overflow-y-auto">
+      <DialogContent className="max-h-[90vh] overflow-y-auto sm:max-w-4xl">
         <DialogHeader>
           <DialogTitle>Manutenção em massa</DialogTitle>
           <DialogDescription>
